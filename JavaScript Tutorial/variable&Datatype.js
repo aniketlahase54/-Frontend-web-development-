5fda91aa1477e47a8786dfd2f53f6237dbd46e68
aniketlahase54 <aniketlahase122@gmail.com>
2026-09-22 13:21:23 +0530

    add variable and data type

---
+++ JavaScript Tutorial/variable&Datatype.js
@@ -0,0 +1,96 @@
+age = 24;
+console.log(age);
+
+price = 99.99;
+console.log(price);
+
+fullname = "Aniket Lahase";
+console.log(fullname);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+isFollow = false;
+console.log(isFollow);
+
+let name = "Aniket Lahase";
+console.log(name);
+
+let age = 24;
+console.log(age);
+
+var name = "Aniket Lahase";
+
+var name = "Amit";
+
+var name = "Rahul";
+console.log(name);
+
+let age=24;
+
+age = 45;
+
+age = 29;
+console.log(age)
+
+const name = "Aniket Lahase"
+name = "Amit";
+
+name = "Rahul";
+
+console.log(name);  //error
+
+const PI = 3.14;
+console.log(PI)
+
+let a;
+a= 10;
+
+console.log(a);
+
+
+// Data Type
+let age = 56;
+console.log(typeof(age))
+
+let price = 99.99;
+console.log(typeof(price));
+
+let name = "Aniket";
+console.log(typeof(name));
+
+x = null;
+console.log(typeof(x));
+
+y = undefined;
+console.log(typeof(y));
+
+isFollow = false;
+console.log(typeof(isFollow));
+
+let num = BigInt("12345");
+console.log(typeof(num));
+
+
+let y = Symbol("Hello....!");
+console.log(typeof(y));
+
+const student = {
+    fullname : "Aniket lahase",
+    age : 23 ,
+    cgpa : 6.85,
+    isPass : true
+};
+
+console.log(student)
+console.log(typeof(student))
+
+console.log(student["fullname"])
+
+student.age = student.age + 1
+
+console.log(student.age)
+console.log(student)
