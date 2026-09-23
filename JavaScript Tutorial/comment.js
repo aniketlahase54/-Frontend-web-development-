012455caf2eb821e6f6762dcd310ddb06cd4b134
aniketlahase54 <aniketlahase122@gmail.com>
2026-09-23 14:07:39 +0530

    add comment is js

---
+++ JavaScript Tutorial/comment.js
@@ -0,0 +1,11 @@
+// single line comment
+// this code prints hellow word
+
+console.log("hello word");
+
+
+// multiline comment
+/*this is a code that prints hello word on our console window */
+
+console.log("Multiline Comment");
+
