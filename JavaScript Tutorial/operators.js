55e616f151714aa351dbd95e5c5a4c7f6ab354fe
aniketlahase54 <aniketlahase122@gmail.com>
2026-09-23 14:46:51 +0530

    add operators in js

---
+++ JavaScript Tutorial/operators.js
@@ -0,0 +1,87 @@
+Arithmetic Operators
+
+let a = 5;
+let b = 4;
+
+console.log("Add :",a + b);
+console.log("Sub :",a - b);
+console.log("Mul :",a * b);
+console.log("Div :",a / b);
+console.log("Mod :",a % b);
+console.log("Power :",a ** b);
+
+Unary Oprators
+let a = 8;
+let b = 6;
+
+post Increment
+
+a++;
+b--;
+console.log(a)
+console.log(b)
+
+Pre Increment
+
+++a;
+--b;
+console.log(a);
+console.log(b);
+
+
+Assignment Oprators
+
+let a = 10;
+let b = 5;
+
+a += 10;
+console.log(a);
+
+a -= 5;
+console.log(a);
+
+a *= 3;
+console.log(a);
+
+a /= 5;
+console.log(a);
+
+a %= 5;
+console.log(a);
+
+a **= 5;
+console.log(a);
+
+Comparison Operators
+
+let a = 9;
+let b = 4;
+
+console.log("9 == 4 :",a == b);
+console.log("9 > 4 :",a > b);
+console.log("9 < 4 :",a < b);
+console.log("9 >= 4 :",a >= b);
+console.log("9 <= 4 :",a <= b);
+console.log("9 != 4 :",a != b);
+
+let a = 9;
+let b = "9";
+
+console.log("a === b:", a===b);
+console.log("a !== b:", a!==b);
+
+Logical Operators
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;
+let cond2 = a === 6;
+
+console.log("cond1 && cond2 :",cond1 && cond2);
+console.log("cond1 || cond2 :", a<b || a === 6);
+console.log("!(a<b):", !(a<b));
+
+
+
+
+
